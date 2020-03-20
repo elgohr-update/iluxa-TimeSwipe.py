addba06c8b786b2c0fb8f7f922b61ffd3945fec6
Ilya Gavrilov <gilyav@gmail.com>
2020-03-20 16:38:36 +0300

    <empty message>

--- build.sh
+++ build.sh
@@ -19,6 +19,8 @@ fi
 if [ "$arch" == "arm64" ]; then
     export arch_name=aarch64
     export plat_name=any
+    # rename package name to *_aarch64
+    sed -i -E "s/name='(.*)'/name='\1_aarch64'/" setup.py
 fi
 curl -s https://api.github.com/repos/panda-official/TimeSwipe/releases/latest | grep "browser_download_url.*timeswipe.*$arch_name.deb" | cut -d : -f 2,3 | tr -d \"  | wget -qi -
 dpkg -i *.deb && rm *.deb && \
@@ -26,10 +28,6 @@ file /usr/lib/libtimeswipe.a && \
 file /usr/lib/libtimeswipe.so.0.0.8 && \
 OVERRIDE_INCLUDES=/usr/include/aarch64-linux-gnu/python3.8\ /usr/include/python3.8 OVERRIDE_LIBS=/usr/lib/libtimeswipe.a\ /usr/lib/libboost_python38.a python3.8 setup.py bdist_wheel --plat-name $plat_name --dist-dir /tmp/dist && python3.8 setup.py clean --all && \
 OVERRIDE_INCLUDES=/usr/include/aarch64-linux-gnu/python3.7\ /usr/include/python3.7 OVERRIDE_LIBS=/usr/lib/libtimeswipe.a\ /usr/lib/libboost_python37.a python3.7 setup.py bdist_wheel --plat-name $plat_name --dist-dir /tmp/dist && python3.7 setup.py clean --all && \
-if [ "$arch" == "arm64" ]; then
-    # rename to timeswipe_aarch64
-    find /tmp/dist -iname '*.whl' -exec sh -c 'x="{}"; mv "$x" `echo $x | sed "s/timeswipe-/timeswipe_aarch64-/"`' \;
-fi
 find /tmp/dist -iname "*.whl" -exec cp {} ${src_dir} \; && \
 dpkg -r timeswipe
 
